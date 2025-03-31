7fbdeea4a9dbc6b156bb1ae51678874abf7721f9
antonelasch <antonelaschneider315@gmail.com>
2025-03-31 10:03:11 -0300

    Commit 1

--- ex9.py
+++ ex9.py
@@ -1,7 +1,7 @@
 salario = int(input("Informe o salário do funcionário: "))
 perc_aumento = int(input("Informe o percentual de aumento: "))
 
-aumento = salario * perc_aumento
+aumento = salario * perc_aumento / 100
 val_aumento = salario + aumento
 
 print("O valor do aumento é", aumento)
